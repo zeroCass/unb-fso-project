6795a1c6c21dd1d2e94ed9663bd770bdb31bb192
Mateus Valerio <mateusvalerio1@gmail.com>
2024-08-07 14:58:29 -0300

    fix: frontend folder link

---
+++ _frontend/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+	title: "FSO Project",
+	description: "Projeto da disciplina FSO da Universidade de Brasília (Unb)",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
+}
