e16dc0b54fa47f72596391447328c717d8fcfa45
Maylla Krislainy <mayllak@hotmail.com>
2024-08-13 22:12:00 -0300

    Config and Desing: Login Interface #4
    Configurações iniciais e implementação visual da interface de login

--- frontend/src/components/login/_loginForm.tsx
+++ frontend/src/components/login/_loginForm.tsx
@@ -1,31 +1,59 @@
-import { getSession, login, logout } from "@/lib/_session";
+import { getSession, login } from "@/lib/_session";
 import { redirect } from "next/navigation";
+import { TextField, Button, Box } from "@mui/material";
+import styles from "./loginForm.module.css";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
 
-export default async function Page() {
+export default async function LoginForm() {
 	const session = await getSession();
 	return (
-		<section>
-			<form
-				action={async (formData) => {
-					"use server";
-					await login(formData);
-					redirect("/");
-				}}
-			>
-				<input type="email" placeholder="Email" />
-				<br />
-				<button type="submit">Login</button>
-			</form>
-			<form
-				action={async () => {
-					"use server";
-					await logout();
-					redirect("/");
-				}}
-			>
-				<button type="submit">Logout</button>
-			</form>
-			<pre>{JSON.stringify(session, null, 2)}</pre>
+		<section className={styles.backgroundBox}>
+			<Box className={styles.styledBox}>
+				<LockOpenIcon fontSize="large" color="primary" />
+
+				<Box className={styles.boxItems}>
+					<form
+						action={async (formData) => {
+							"use server";
+							await login(formData);
+							redirect("/");
+						}}
+					>
+						<TextField
+							fullWidth
+							label="CPF"
+							type="cpf"
+							placeholder="CPF"
+							margin="normal"
+							variant="outlined"
+						/>
+
+						<TextField
+							fullWidth
+							label="Password"
+							type="password"
+							placeholder="Password"
+							margin="normal"
+							variant="outlined"
+							className={styles.admPassword}
+						/>
+
+						<Box mt={2}>
+							<Button
+								fullWidth
+								variant="contained"
+								color="primary"
+								type="submit"
+							>
+								Login
+							</Button>
+						</Box>
+					</form>
+				</Box>
+				<Button className={styles.admlogin} variant="text">
+					logar como administrador
+				</Button>
+			</Box>
 		</section>
 	);
 }
