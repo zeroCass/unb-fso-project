33f46f9a570db0e6329c4b66f8c719928186e50a
Maylla Krislainy <mayllak@hotmail.com>
2024-09-01 00:34:47 -0300

    style: tela de matricular-se em turma #1
    Inicio dos ajustes e estilização

--- frontend/src/components/turmas/TurmaItem.tsx
+++ frontend/src/components/turmas/TurmaItem.tsx
@@ -1,45 +1,65 @@
 "use client";
 
-import matricular from "@/actions/matricular";
 import { Turma } from "@/types";
-import { Box, Button, Paper, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
+import styles from "./TurmaItem.module.css";
 
-export default function TurmaItem({ turma }: { turma: Turma }) {
-	const reazliarMatricular = async () => {
-		await matricular(turma.id);
-	};
-
+export default function TurmaItem({
+	turma,
+	selected,
+	disabled,
+	onSelect,
+}: {
+	turma: Turma;
+	selected: boolean;
+	disabled: boolean;
+	onSelect: () => void;
+}) {
 	return (
 		<Paper
-			elevation={16}
-			sx={{
-				width: "500px",
-				height: "250px",
-			}}
+			elevation={selected ? 16 : 4}
+			className={`${styles.turmaItem} ${selected ? styles.selected : ""} ${
+				disabled ? styles.disabled : ""
+			}`}
+			onClick={!disabled ? onSelect : undefined}
+			sx={{ flexGrow: 1, height: "100%" }}
 		>
-			<Box bgcolor="#165DBD" color="#fff" display="flex" flexDirection="column" height="100%">
-				<Box display="flex" alignItems="center" justifyContent="center" flexDirection="column">
-					<Typography>{turma.trilha}</Typography>
-					<Typography>Descrção da trilha</Typography>
+			<Box
+				display="flex"
+				flexDirection="column"
+				height="100%"
+				justifyContent="space-around"
+			>
+				<Box
+					display="flex"
+					alignItems="center"
+					justifyContent="center"
+					flexDirection="column"
+				>
+					<Typography variant="h6">{turma.trilha}</Typography>
+					<Typography variant="body2">Descrição da trilha</Typography>
 				</Box>
-				<Box display="flex" justifyContent="space-between">
-					<Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" width="50%">
-						<Typography variant="h3" fontSize={26}>
-							Turma
-						</Typography>
-						<Typography>{turma.nome}</Typography>
+				<Box display="flex">
+					<Box
+						display="flex"
+						alignItems="center"
+						justifyContent="center"
+						flexDirection="column"
+						width="50%"
+					>
+						<Typography variant="h5">Turma</Typography>
+						<Typography variant="subtitle1">{turma.nome}</Typography>
 					</Box>
-					<Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" width="50%">
-						<Typography variant="h3" px="20px">
-							Vagas
-						</Typography>
-						<Typography>{turma.capacidadeAtual}</Typography>
+					<Box
+						display="flex"
+						alignItems="center"
+						justifyContent="center"
+						flexDirection="column"
+						width="50%"
+					>
+						<Typography variant="h5">Vagas</Typography>
+						<Typography variant="subtitle1">{turma.capacidadeAtual}</Typography>
 					</Box>
-				</Box>
-				<Box>
-					<Button sx={{ color: "#fff" }} onClick={() => reazliarMatricular()}>
-						Escolher
-					</Button>
 				</Box>
 			</Box>
 		</Paper>
